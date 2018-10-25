17148998c77de48f7e885f0c13f025696807a339
Mohit54 <mohitmali54@gmail.com>
2018-10-25 21:00:15 +0530

    Add files via upload

---
+++ login1.php
@@ -0,0 +1,17 @@
+<?php
+$name=$_POST['name'];
+$password=$_POST['password'];
+$conn=mysqli_connect('localhost','root','') or die(mysql_error());
+mysqli_select_db($conn,'balance') or die(mysql_error());
+$sql="select  * from bank1 where name='$name' and password='$password' ";
+$result=mysqli_query($conn,$sql) ;
+if(mysqli_num_rows($result)>0)
+{
+$row=mysqli_fetch_assoc($result);
+echo $row["name"];
+echo "welcome";
+header("Location:E-learning.html");
+exit();
+}
+else
+echo "password does not match";?>
---
+++ pay1.php
@@ -0,0 +1,33 @@
+<?php
+$user_name=$_POST['user_name'];
+$name=$_POST['name'];
+$password=$_POST['password'];
+
+
+if( $user_name== "")
+         {
+
+	echo "Card holder name cant be null";
+	}
+elseif($name== ""){
+	echo "email cant be null";
+
+}
+elseif($password== ""){
+	echo "password cant be null";
+
+}
+
+
+
+else{
+
+$conn=mysqli_connect('localhost','root','') or die(mysql_error());
+mysqli_select_db($conn,'balance')or die(mysql_error());
+$sql="INSERT INTO bank VALUES ('$user_name','$name','$password')";
+mysqli_query($conn,$sql) ;
+header("Location:login.html");
+	exit();
+		
+}
+?>
